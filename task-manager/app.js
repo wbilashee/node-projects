57de12de2c4d2951ac947ce0bab4dc48034df71f
Habiba Akter Bilashee <wbilashee@gmail.com>
2022-11-21 22:36:28 +0600

    tasks api done

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,10 +1,31 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
 
-app.get("/", (req, res) => {
-    res.send("Home page");
-});
+// middleware
+app.use(express.json());
+app.use(express.static("/public"));
 
-app.listen(5000, () => {
-    console.log("Server is listening on port 5000...");
-});
+// routes
+app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
---
+++ task-manager/middleware/error-handler.js
@@ -0,0 +1,11 @@
+const { CustomError } = require("../errors/custom-error");
+
+const errorHandler = (err, req, res, next) => {
+    if (err instanceof CustomError) {
+        return res.status(err.statusCode).json({ msg: err.message });
+    }
+
+    return res.status(500).json({ msg: "Something went wrong, please try again" });
+}
+
+module.exports = errorHandler;
