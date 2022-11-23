e2e258b4a4fc08efba22b6c0d6112167ae3c3cab
Habiba Akter Bilashee <wbilashee@gmail.com>
2022-11-23 13:50:48 +0600

    server done

--- jwt-basics/app.js
+++ jwt-basics/app.js
@@ -2,6 +2,29 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
+const authRouter = require("./routes/auth");
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
+
+// routes
+app.use("/api/v1", authRouter);
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
---
+++ jwt-basics/routes/auth.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+    login,
+    dashboard,
+} = require("../controllers/auth");
+const authMiddleware = require("../middleware/auth");
+
+router.route("/login").post(login);
+router.route("/dashboard").get(authMiddleware, dashboard);
+
+module.exports = router;
